736f07b806de9fcb44f019b1f15a6e05abe0b257
GP-TI <gp-ti@grupoproser.com>
2026-06-09 09:41:42 -0500

    Autorizar Maria Garcias y Gabriel Moreno en Catalogos Express

--- src/config/expressCatalogosPermitidos.js
+++ src/config/expressCatalogosPermitidos.js
@@ -4,6 +4,8 @@
  */
 export const LOGIN_CATALOGOS_EXPRESS_EXTRA = [
   '1048210029', // Alexander Escalante
+  '1007183772', // Maria Garcias
+  '1044800214', // Gabriel Moreno
 ];
 
 export const EMAILS_CATALOGOS_EXPRESS_EXTRA = [
